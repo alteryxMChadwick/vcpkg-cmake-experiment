cafeb5c2e3cade7fa091fbf8f4c485b32cb73a7c
Michael Chadwick <mchadwick@alteryx.com>
2019-09-19 15:39:56 -0600

    Works even more
    Changes to be committed:
    	modified:   .gitignore
    	modified:   MyWSLToolchain.cmake
    	modified:   build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,17 @@
 #   -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake \
 #   -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=../my/project/compiler-settings-toolchain.cmake
 
-cmake . -DAUTO_VCPKG_ROOT=/w/vcpkg -DCMAKE_TOOLCHAIN_FILE=${AUTO_VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=./MyWSLToolchain.cmake
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CMAKE_DIR=$SCRIPTPATH/build-dir/linux/cmake
+
+mkdir -p $CMAKE_DIR
+cd $CMAKE_DIR
+
+VCPKG_ROOT=/w/vcpkg
+
+cmake -G "Unix Makefiles" $SCRIPTPATH \
+    -DAUTO_VCPKG_ROOT=$VCPKG_ROOT \
+    -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$SCRIPTPATH/MyWSLToolchain.cmake
+
+cd $SCRIPTPATH
