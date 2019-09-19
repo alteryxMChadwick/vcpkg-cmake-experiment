b757bba61ba0488857b20e1b02d1184ea2ed572a
Michael Chadwick <mchadwick@alteryx.com>
2019-09-19 16:06:12 -0600

    Builds and runs a test
    Changes to be committed:
    	modified:   CMakeLists.txt
    	modified:   MyWSLToolchain.cmake
    	new file:   _test/catch-test.cpp
    	new file:   _test/main.cpp
    	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 
-# cmake ../my/project \
-#   -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake \
-#   -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=../my/project/compiler-settings-toolchain.cmake
-
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CMAKE_DIR=$SCRIPTPATH/build-dir/linux/cmake
 
@@ -13,7 +9,9 @@ VCPKG_ROOT=/w/vcpkg
 
 cmake -G "Unix Makefiles" $SCRIPTPATH \
     -DAUTO_VCPKG_ROOT=$VCPKG_ROOT \
+    -DVCPKG_TEST_ROOT=$SCRIPTPATH \
     -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$SCRIPTPATH/MyWSLToolchain.cmake
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$SCRIPTPATH/MyWSLToolchain.cmake \
+    && make && ./vcpkg_test
 
 cd $SCRIPTPATH
